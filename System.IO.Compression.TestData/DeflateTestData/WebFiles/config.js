1b409ad29feded5e1723e67a37fe094c1c8d0a08
Ian Hays <ianha@microsoft.com>
2017-12-05 11:15:14 -0800

    Add more tests

---
+++ System.IO.Compression.TestData/DeflateTestData/WebFiles/config.js
@@ -0,0 +1,4 @@
+µUmoÛ6þ+?µE½Z/.ú¡‰»®YÜx±Ó`ƒ–N[‹TIÊNä¿ïè¸Žœ-€€bßÄãŸ—£Ž§——“«E!EÅWæ­€­3f^½¦+0s^ã×›S›B5hÍ¥ ÇÎ[‡äq¹_únå±§1¸Y–nV.“|ð“¤$ûJV–Ÿ™zåÜ“[ú¾“Ég¼s§g¿ÿ6uÏ®‡ÉÜ‡×ãÜýßœ Ì+1«æ…’ZV†²ÞEqž…Ã$ò4Jl¬P˜š•ÀB6Œ£ .˜_$C¶L“"©|Wqé‡~:„eÎÈƒóšrÁe£¹E)kÆÅ -¡+‹V­qÓó¢¤
+ü,ŠhÝhÃmµ‡I¬5RµXnT‡.¨ÃÂÄÓ2º' Ør(«bkr5·á½«¸kP[æûv´lUh?«£bƒS¶‚)S¬Öv·ÁÕ%ÛWÞó½¤|Öj³ÒØD@FF·\”rK¶†ü…*–sÐ¦GõBK†êßîÊŠ]ÎŒâ…->„ì½QÇ	,ÆÂó™ƒ°b-Y‰g"¸##ôföLñ€¬Ù¬ïçùAX…í'
+¾µHv¶[Ð™rÔÞ{ücËniôÈól·Ÿ+–Iî‰[k{LÙW†½ÿJ*Àx'f¨Ê[èÂû¢½ò08Ð¿aÜ8VÕùð@þÛðbHñý°/ýÇhw>¿¼x‚ŒûCÆHqH>FµÍx¿aþ;¹ùÅ9›Íž³¾Ê~¦SLk0šþh­µW”8—Ö =¸Ã+(ØÚ«·•wB­Ÿûä¸Î”\AažˆGý-‹:Ô+0Åm_·>c‚TÎyÇ¯ ¯_ÁÏøµÝnéÑÔò¶°Ä9!JOßÊ¦A@¼Õ›=úåÈ°Ù~ÿˆuÒ—uòÿ³þñùŒ÷ørâ|´Ó•†o:¿cÚ¿Ñi_….L‡Á¯óÉ…ƒ³WwÀ‡ðkêØaw˜†ö$S¯Ç­Ú¡w¦R×
+=8y…yGá3%/]çÃì#¨°OÒ~¢có¦ÌØŸohöïÎþ)jQñ‚ããE‚( ŸÒ i%ö©}ó
